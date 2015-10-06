1bf1b2dcc031c1de63953d9b9ba33e7d57015d72
Jordan Hall <jordan.hall@rapidweb.biz>
2015-10-06 11:43:11 +0100

    Security token comparison and deployment lock file checks

--- deploy.php
+++ deploy.php
@@ -1,5 +1,14 @@
 <?php
 
+function deleteLockFile($lockFile)
+{
+    $result = unlink($lockFile);
+    
+    if (!$result) {
+        throw new \Exception("Unable to delete the lock file. Deployment will be restricted until the lock file is removed.");
+    }
+}
+
 $config = json_decode(file_get_contents(".config.json"));
 
 if (!$config) {
@@ -12,6 +21,28 @@ if ($config->security->token=="replace_me_with_a_random_string") {
 
 if (!trim($config->security->token)) {
     throw new \Exception("The security token is currently empty. Please change this in the configuration file.");
+}
+
+if (!isset($_GET['st'])) {
+    throw new \Exception("No `st` parameter in URL. This is required.");
+}
+
+if ($_GET['st']!=$config->security->token) {
+    throw new \Exception("Security token is invalid. Change your request to match the security token in the configuration file.");
+}
+
+if (!trim($config->files->lock)) {
+    throw new \Exception("The lock file is currenty empty. Please change this in the configuration file.");
+}
+
+if (file_exists($config->files->lock)) {
+    throw new \Exception("The lock file currently exists, suggesting the script is currently running. Deployment will be restricted until the lock file is removed.");
+}
+
+$result = file_put_contents($config->files->lock, time());
+
+if (!$result) {
+    throw new Exception("There was an error writing to the lock file. Check the user running this script has write permissions on the containing directory.");
 }
 
 $headers = getallheaders();
@@ -42,14 +73,17 @@ foreach($headers as $name => $value) {
 }
 
 if ($webhookSource == null) {
+    deleteLockFile($config->files->lock);
     throw new \Exception("Unable to identify source of webhook request. This deployment script accepts requests from GitHub and GitLab web hooks only.");
 }
 
 if ($webhookType == null) {
+    deleteLockFile($config->files->lock);
     throw new \Exception("Unable to identify type of webhook request. This deployment script accepts push webhook requests only.");
 }
 
 if ($webhookType != "push") {
+    deleteLockFile($config->files->lock);
     throw new \Exception("This does not appear to be a push webhook request. This deployment script accepts push webhook requests only.");
 }
 
