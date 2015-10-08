27499b035a14a676bb326bac65460e33d82b9190
Jordan Hall <jordan.hall@rapidweb.biz>
2015-10-08 12:47:21 +0100

    Debugging and fixes from local testing

--- deploy.php
+++ deploy.php
@@ -9,6 +9,13 @@ function deleteLockFile($lockFile)
     }
 }
 
+function exceptionHandler($exception) {
+    $exceptionFileName = "/tmp/deploymentException_".time().".txt";
+    file_put_contents($exceptionFileName, print_r($exception, true));
+}
+
+set_exception_handler('exceptionHandler');
+
 $config = json_decode(file_get_contents(".config.json"));
 
 if (!$config) {
@@ -44,6 +51,12 @@ $result = file_put_contents($config->files->lock, time());
 if (!$result) {
     throw new Exception("There was an error writing to the lock file. Check the user running this script has write permissions on the containing directory.");
 }
+
+/*$result = chmod($config->files->lock, 0777);
+
+if (!$result) {
+    throw new Exception("There was an error changing the permissions of the lock file.");
+}*/
 
 $webhookSource = null;
 $webhookEvent = null;
@@ -90,71 +103,74 @@ if (isset($config->security->checkWebhookHeaders) && $config->security->checkWeb
     }
 }
 
-$request = json_decode(http_get_request_body());
+$requestBody = file_get_contents('php://input');
+
+$request = json_decode($requestBody);
 
 if (!$request) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("Web hook request body was not present or is invalid JSON.");
 }
 
 if (!trim($config->git->branch)) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("The git branch specified in the configuration file is currenty blank. Please change this in the configuration file.");
 }
 
 $fullGitRef = "refs/heads/".$config->git->branch;
 
 if ($request->ref != $fullGitRef) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("This push was not for the configured branch, therefore it is being ignored. If this is incorrect, please check the branch defined in the configuration file.");
 }
 
 if (!trim($config->git->repositoryUrl)) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("The git repository url specified in the configuration file is currenty blank. Please change this in the configuration file.");
 }
 
 if (!trim($config->directories->temporary)) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("The temporary directory specified in the configuration file is currenty blank. Please change this in the configuration file.");
 }
+
+$commands = array();
 
 if (!is_dir($config->directories->temporary)) {
 
     if (!mkdir($config->directories->temporary)) {
-        deleteLockFile();
+        deleteLockFile($config->files->lock);
         throw new \Exception("The temporary directory could not be created. Check the user running this script has write permissions on the containing directory.");
     }
     
-    $commands[] = printf("git clone --depth=1 --branch %s %s %s", $config->git->branch, $config->git->repositoryUrl, $config->directories->temporary);
+    $commands[] = sprintf("git clone --depth=1 --branch %s %s %s", $config->git->branch, $config->git->repositoryUrl, $config->directories->temporary);
 }
 else
 {
-    $commands[] = sprintf("git --git-dir=\"%s.git\" --work-tree=\"%s\" fetch origin %s", $config->directories->temporary, $config->directories->temporary, $config->git->branch);
-	$commands[] = sprintf('git --git-dir=\"%s.git\" --work-tree=\"%s\" reset --hard FETCH_HEAD', $config->directories->temporary, $config->directories->temporary);
+    $commands[] = sprintf('git --git-dir="%s.git" --work-tree="%s" fetch origin %s', $config->directories->temporary, $config->directories->temporary, $config->git->branch);
+	$commands[] = sprintf('git --git-dir="%s.git" --work-tree="%s" reset --hard FETCH_HEAD', $config->directories->temporary, $config->directories->temporary);
 }
 
 if (!chdir($config->directories->temporary)) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("The current directory could not be changed to the specified temporary directory.");
 }
-
 
 if ($config->git->updateSubmodules) {
     $commands[] = "git submodule update --init --recursive";
 }
 
 if (!trim($config->directories->deployment)) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("The deployment directory specified in the configuration file is currenty blank. Please change this in the configuration file.");
 }
 
 if (!is_dir($config->directories->deployment)) {
-    deleteLockFile();
+    deleteLockFile($config->files->lock);
     throw new \Exception("The deployment directory does not seem to exist. Please create the directory or check the directory specified in the configuration file..");
 }
 
-$commands[] = sprintf('rsync -rltgoDzvO %s %s', $config->directories->temporary, $config->directories->deployment);
+$commands[] = sprintf('rsync -rltDzvO %s %s', $config->directories->temporary, $config->directories->deployment);
 
 foreach ($commands as $command) {
     
@@ -165,9 +181,11 @@ foreach ($commands as $command) {
 	exec($command." 2>&1", $output, $returnCode);
 	
 	if ($returnCode !== 0) {
-	    deleteLockFile();
-		throw new \Exception("Deployment error - Return code ".$returnCode." received when attempting to run command: ".$command);
+	    deleteLockFile($config->files->lock);
+		throw new \Exception("Deployment error - Return code ".$returnCode." received when attempting to run command: ".$command." - ".print_r($output, true));
 	}
 }
+
+deleteLockFile($config->files->lock);
 
 ?>
